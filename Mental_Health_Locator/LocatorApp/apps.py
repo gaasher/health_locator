e21e7e15b5b37e62290e2618f558224a6e3347f4
Gabriel Asher <85761680+gaasher@users.noreply.github.com>
2021-12-31 16:39:47 -0500

    Delete Mental_Health_Locator directory
    remove dir copy

--- Mental_Health_Locator/LocatorApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LocatorappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'LocatorApp'
--- Mental_Health_Locator/RetrievalApp/urls.py
+++
@@ -1,9 +0,0 @@
-from os import stat
-from django.conf.urls import url
-from django.urls.conf import path
-from RetrievalApp import views
-
-
-urlpatterns = [
-    url(r'^results/$', views.RetrievalAPI)
-]
